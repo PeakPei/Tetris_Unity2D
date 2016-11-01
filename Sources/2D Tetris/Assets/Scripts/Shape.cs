7874af9d74c32696bc8b77554964fb2db1e4d7f5
victoria-viver <victoria.shenk@gmail.com>
2016-11-01 06:12:57 +0200

    Tetris 1.0

---
+++ Sources/2D Tetris/Assets/Scripts/Shape.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Shape : MonoBehaviour 
+{
+	float lastFallStepTime = 0f;	
+	float speed = 1f;
+
+	int vBound = 0;
+	int hBound = 0;
+
+	List<Vector3> nonValidPositions = new List<Vector3>();
+ 
+	void Update() 
+	{
+		if (Input.GetKeyDown(KeyCode.RightArrow)) 
+		{
+			transform.position += Vector3.right;
+
+			if (!isValidPosition())
+				transform.position += Vector3.left;	
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left;
+
+			if (!isValidPosition())
+				transform.position += Vector3.right;
+		}
+		else if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			transform.Rotate(0, 0, 90);
+
+			if (!isValidPosition())
+				transform.Rotate(0, 0, -90);
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow) ||
+		         Time.time - lastFallStepTime >= speed) 
+		{
+			transform.position += Vector3.down;
+
+			if (!isValidPosition())
+			{
+				transform.position += Vector3.up;
+				
+				bool isGameOver = (transform.position.y >= vBound ? true : false);
+				Camera.main.GetComponent <Tetris>().Next(isGameOver);
+				enabled = false;
+			}
+
+			lastFallStepTime = Time.time;
+		}
+	}
+
+	public void Init (int boundV, int boundH, List<Vector3> nonValidPos)
+	{
+		vBound = boundV;
+		hBound = boundH;
+
+		nonValidPositions = nonValidPos;
+	}
+
+	bool isValidPosition() 
+	{        
+		foreach (Transform child in transform) 
+		{
+			Vector3 v = RoundPos(child.position);
+
+			if (!isInsideGrid(v))
+				return false;
+			else 
+			{
+				for (int i = 0; i < nonValidPositions.Count; i++) 
+				{
+					if (child.position == nonValidPositions[i])
+					{
+						return false;
+					}
+				}
+			}
+		}
+
+		return true;
+	}
+
+	Vector3 RoundPos(Vector3 v) 
+	{
+		return new Vector3(v.x + .5f, v.y - .5f, 0);
+	}
+
+	bool isInsideGrid(Vector3 pos) 
+	{
+		return (   pos.x >= -hBound + 1
+				&& pos.x <= hBound
+				&& pos.y >= -vBound);
+	}
+}
---
+++ Sources/2D Tetris/Assets/Scripts/Tetris.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tetris : MonoBehaviour 
+{
+	float offset = .5f;
+
+	int gameHeight = 0;
+	int gameWidth = 0;
+
+	int score = 0;
+
+	List<Vector3> partsPositions = new List<Vector3>();
+
+	public GameObject[] shapesTypesList;
+	public GameObject boundL;
+	public GameObject boundR;
+
+	public Text ScoreText;
+
+	void Awake ()
+	{
+		gameHeight = (int) Camera.main.orthographicSize * 2;
+		gameWidth = (int) Camera.main.orthographicSize * Screen.width / Screen.height * 2;
+
+		boundL.transform.localScale = new Vector3 (0.1f, gameHeight, 0);
+		boundR.transform.localScale = new Vector3 (0.1f, gameHeight, 0);
+		boundL.transform.position = new Vector3 (-gameWidth/2 - boundL.transform.localScale.x/2, 0, 0);
+		boundR.transform.position = new Vector3 ( gameWidth/2 + boundL.transform.localScale.x/2, 0, 0);
+
+		ScoreText.transform.position = new Vector3 (0, Camera.main.orthographicSize - 1.5f, 0);
+	}
+
+	void Start () 
+	{
+		UpdateScore ();
+		Add ();
+	}
+
+	void Add ()
+	{
+		int i = Random.Range(0, shapesTypesList.Length);
+
+		GameObject shape = Instantiate ( shapesTypesList[i], 
+											   new Vector2 (1, gameHeight/2+2), 
+											   Quaternion.identity ) as GameObject;
+
+		Shape shapeScript = shape.GetComponent<Shape>();
+
+		shapeScript.Init (gameHeight/2, gameWidth/2, partsPositions);
+	}
+
+	public void Next (bool isGameOver)
+	{
+		if (!isGameOver)
+		{
+			UpdatePartsPositions ();
+			CheckForFull ();
+			StartCoroutine (AddNextAfterEndOfFrame());			
+		}
+	}
+
+	IEnumerator AddNextAfterEndOfFrame ()
+	{
+		yield return new WaitForEndOfFrame();
+		UpdatePartsPositions ();
+		Add ();
+	}
+
+	void UpdatePartsPositions ()
+	{
+		GameObject[] parts = GameObject.FindGameObjectsWithTag("Part");
+		partsPositions = new List<Vector3>(); 
+
+		foreach (GameObject part in parts) 
+		{
+			partsPositions.Insert (partsPositions.Count, part.transform.position);
+		}
+	}
+
+	void CheckForFull ()
+	{
+		for (int l = GetMaxLineID(); l > -gameHeight/2; l--) 
+		{
+			if (isFullLine(l)) 
+				DeleteLine (l);
+		}
+	}
+
+	void DeleteLine (int lineID)
+	{
+		GameObject[] parts = GameObject.FindGameObjectsWithTag("Part");
+
+		foreach (GameObject part in parts)
+		{
+			int posY = (int) Mathf.Round (part.transform.position.y + offset);
+
+			if (posY == lineID)
+		    	Destroy(part);
+		    else if (posY > lineID)
+		    	part.transform.position += Vector3.down;
+		}
+
+		score++;
+		UpdateScore ();
+	}
+
+	int GetMaxLineID ()
+	{
+		int maxLineID = -gameHeight/2 - 1;
+
+		for (int i = 0; i < partsPositions.Count; i++) 
+		{
+			int posY = (int) Mathf.Round (partsPositions[i].y - offset);
+
+			if (posY > maxLineID)
+			{
+				maxLineID = posY;
+			}			
+		}
+
+		return maxLineID;
+	}
+
+	bool isFullLine (int lineID)
+	{
+		int count = 0;
+
+		for (int i = 0; i < partsPositions.Count; i++) 
+		{
+			int posY = (int) Mathf.Round (partsPositions[i].y + offset);
+			
+			if (posY == lineID)
+				count++;
+		}
+		
+		return (count == gameWidth);
+	}
+
+	void UpdateScore () 
+	{
+		ScoreText.text = score.ToString();
+	}
+}
